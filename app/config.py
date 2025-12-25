d26cb241e50d94a5ca9718374f367ae879b13a28
dusnila <davidovich18062002@gmail.com>
2025-12-25 22:17:47 +0000

    add migration

---
+++ app/config.py
@@ -0,0 +1,20 @@
+from pydantic import root_validator
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Setting(BaseSettings):
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+    @property
+    def DB_URL(self) -> str:
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+
+setting = Setting()
+
+print(setting.DB_URL)
--- app/database.py
+++ app/database.py
@@ -1,15 +1,9 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-DB_HOST = "localhost"
-DB_PORT = 5432
-DB_USER = "postgres"
-DB_PASS = "postgres"
-DB_NAME = "postgres"
+from app.config import setting
 
-DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-
-engin = create_async_engine(DATABASE_URL)
+engin = create_async_engine(setting.DB_URL)
 
 async_session_maker = sessionmaker(engin, class_=AsyncSession, expire_on_commit=False)
 
