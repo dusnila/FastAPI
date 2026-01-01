dee6ee89108f2215f51a7a1e271135c9f3771be8
dusnila <davidovich18062002@gmail.com>
2026-01-01 11:19:41 +0000

    add models and migration

--- app/migrations/env.py
+++ app/migrations/env.py
@@ -12,6 +12,10 @@ sys.path.insert(0, dirname(dirname(abspath(__file__))))
 from app.config import setting
 from app.database import Base
 from app.hotels.models import Hotels
+from app.users.models import Users
+from app.rooms.models import Rooms
+from app.booking.models import Bookings
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
