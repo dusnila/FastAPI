fe42ca9b545dd6b53a13282448c763ac179675d0
dusnila <davidovich18062002@gmail.com>
2026-01-07 21:27:59 +0000

    register & login

---
+++ app/users/auth.py
@@ -0,0 +1,31 @@
+from datetime import datetime, timedelta
+from passlib.context import CryptContext
+from jose import jwt
+from pydantic import EmailStr
+
+from app.config import Setting
+from app.users.service import UsersService
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def get_password_hash(password: str) -> str:
+    return pwd_context.hash(password)
+
+def verify_password(plain_password, hashed_password) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    setting = Setting()
+    expire = datetime.now() + timedelta(minutes=30)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(
+        to_encode, setting.SALT
+    )
+    return encoded_jwt
+
+async def authenticate_user(email: EmailStr, password: str):
+    user = await UsersService.find_one_or_not(email=email)
+    if not user and not verify_password(password, user.password):
+        return None
+    return user
